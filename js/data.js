4af2490195e9b3ba839ddb6868c62ed3326fcb8e
anhelikscbr1 <123397822+anhelikscbr1@users.noreply.github.com>
2023-05-09 08:40:16 -0600

    ultimo commit  :)

---
+++ js/data.js
@@ -0,0 +1,33 @@
+const songs = [
+    {
+        name: 'La eneida',
+        path: './music/Song1.mp3',
+        artist: 'Los Panchos',
+        cover: './images/cover1.png'
+    },
+    {
+        name: 'song 2',
+        path: './music/Song2.mp3',
+        artist: 'BZRP',
+        cover: './images/cover2.png'
+    },
+    {
+        name: 'La tuza',
+        path: './music/Song3.mp3',
+        artist: 'Esta biena y abuza',
+        cover: './images/cover3.png'
+    },
+    {
+        name: 'paseme por favor',
+        path: './music/Song4.mp3',
+        artist: 'perdon por no ir a clase',
+        cover: './images/cover4.png'
+
+    },
+    {
+        name: 'Querer querernos',
+        path: './music/Song5.mp3',
+        artist: 'Cancerbero',
+        cover: './images/cover5.png'
+    }
+]
